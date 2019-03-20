8c406aaf55b19ad9abfd9114cdda2f0a865dde68
Mauricio Gamboa Vasquez <mauricio.gamboa@avantica.net>
2019-03-19 22:44:05 -0700

    removed comments

--- app/src/main/java/com/abc/helloword/MainActivity.java
+++ app/src/main/java/com/abc/helloword/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
      EditText editText = findViewById(R.id.text_message);
       intent.putExtra("MESSAGE",  editText.getText().toString());
-//
+
       startActivity(intent);
     }
 }
